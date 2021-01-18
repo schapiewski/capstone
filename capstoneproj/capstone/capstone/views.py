3108d6f8a44fdf79e99d829480823a3bd6063420
schapiewski <schapiewski@oakland.edu>
2021-01-18 15:25:34 -0500

    Basic foundation with login
    Basic Django foundation with login functionality

---
+++ capstoneproj/capstone/capstone/views.py
@@ -0,0 +1,45 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+
+from django.contrib.auth.decorators import login_required
+
+from .forms import CreateUserForm
+
+def register(request):
+    form = CreateUserForm()
+
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            user = form.cleaned_data.get('username')
+            messages.success(request, 'Account was created for ' + user)
+            return redirect('../login/')
+    context = {'form': form}
+    return render(request, 'register.html', context)
+
+def loginPage(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('../')
+        else:
+            messages.info(request, 'Username or password is incorrect')
+            return render(request, 'login.html')
+    context = {}
+    return render(request, 'login.html', context)
+
+def logoutUser(request):
+    logout(request)
+    return redirect('login')
+
+@login_required(login_url='login')
+def dashboard(request):
+    context = {}
+    return render(request, 'dashboard.html', context)
