2d8934ec366af9c0995ef4ddb44771402fab568f
Alex Kocab <46122733+dkkocab@users.noreply.github.com>
2021-02-16 19:55:32 -0500

    Delete capstoneproj/capstone directory

--- capstoneproj/capstone/assets/javascript/register-placeholders.js
+++
@@ -1,9 +0,0 @@
-var form_fields = document.getElementsByTagName('input')
-      form_fields[1].placeholder='Username...';
-      form_fields[2].placeholder='Email...';
-      form_fields[3].placeholder='Enter Password...';
-      form_fields[4].placeholder='Re-Enter Password...';
-
-      for (var field in form_fields) {
-      form_fields[field].className += 'form-control'
-      }
